326a4fc82d46b78dc4ec4b0fa47db9a62b93145f
noahbaum2231 <113562708+noahbaum2231@users.noreply.github.com>
2022-09-27 18:50:44 -0400

    Create Assignment 1.py

---
+++ Assignment 1.py
@@ -0,0 +1,17 @@
+#2
+# x = 6 
+#print(6)
+#print("6")
+#ANSWER print(6) will give 6 but "6" is string and will not work
+
+#5
+#What happens if you attempt to use a variable within a program, and that variable has not been assigned a value?
+#ANSWER Trying to use a variable that has not been assigned a value will result in error of it being undefined
+
+#6
+#What is wrong with the following statement that attempts to assign the value ten to variable x?
+#10 = x 
+#ANSWER when assigning a variable it must be one variable on left side as shown x = 10 would be correct
+
+#17
+print('A\nB\nC\nD\nE\nF\n')
