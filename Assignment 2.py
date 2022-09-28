77af68b4f0731e451a21a4fb9e2abe4cfa45f152
noahbaum2231 <113562708+noahbaum2231@users.noreply.github.com>
2022-09-28 11:03:06 -0400

    Create Assignment 2.py

---
+++ Assignment 2.py
@@ -0,0 +1,36 @@
+#3 What is an integer equivalent to True in Python?
+1 == True 
+
+#4 What is the integer equivalent to False in Python?
+0 == False
+
+#6 Given the following definitions:
+# x, y, z = 3, 5, 7
+#A True
+#B True
+#C False
+#D True
+#E False
+#F True
+#G Ture
+#H False
+#I False
+#J True
+#K True
+#L False
+
+#9 Express the following Boolean expressions in an equivalent form without the not. x and y are integers.
+#A 
+#B
+#C
+#D
+#E
+#F
+#G
+#H
+#I
+#J
+
+x = 3
+y = 5
+not (x==y)
